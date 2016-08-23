bb32392a924f9e02b5ec76bd9513a404cdc48700
Nessalc <jclassen@gmail.com>
2016-08-23 16:14:03 -0500

    Added login form

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, {'template_name':'login.html'}),
+    url(r'^', include('django.contrib.auth.urls')),
 ]
--- mysite/myuser/views.py
+++ mysite/myuser/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def login_view(request):
+    username=request.POST['username']
+    password=request.POST['password']
+    user=authenticate(username=username,password=password)
+    if user is not None:
+        login(request,user)
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+    else:
+        return redirect('/login')
+
+def logout_view(request):
+    logout(request)
